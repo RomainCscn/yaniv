0a9236b02618468dc3c303dc5de38b822444d2cc
Romain Cascino <contact@romaincascino.com>
2020-11-13 08:41:35 +0100

    feat(client): Add copy link button, improve button styles

--- client/src/components/Lobby/ShareLink/index.tsx
+++ client/src/components/Lobby/ShareLink/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
-import styles from '../styles.module.css';
+import styles from './styles.module.css';
 
-const ShareLink = () => (
-  <div>
-    <p className={styles.linkText}>Partagez ce lien à vos amis pour qu'ils vous rejoignent</p>
-    <p className={styles.link}>
-      <a href={window.location.href}>{window.location.host + window.location.pathname}</a>
-    </p>
-  </div>
-);
+const ShareLink = () => {
+  const copyLink = () => {
+    const textField = document.createElement('textarea');
+    textField.innerText = window.location.href;
+    document.body.appendChild(textField);
+    textField.select();
+    document.execCommand('copy');
+    textField.remove();
+  };
+
+  return (
+    <div>
+      <p className={styles.linkText}>Partagez ce lien à vos amis pour qu'ils vous rejoignent</p>
+      <div className={styles.linkContainer}>
+        <a className={styles.link} href={window.location.href}>
+          {window.location.host + window.location.pathname}
+        </a>
+        <button className={styles.copyButton} onClick={() => copyLink()}>
+          Copier
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default ShareLink;
