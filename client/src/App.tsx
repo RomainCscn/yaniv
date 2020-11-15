98e7c7d9aa63123248b37f737fae2cbb5df4d7f5
Romain Cascino <contact@romaincascino.com>
2020-11-15 17:56:40 +0100

    chore: Add ackee for analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, useLocation } from 'react-router-dom';
+import useAckee from 'use-ackee';
 
 import { SUITS, VALUES } from './constants';
 import Lobby from './components/Lobby';
 import { getCardImagePath } from './core/utils';
 
 const App = () => {
+  const location = useLocation();
+  useAckee(
+    location.pathname,
+    {
+      server: 'https://gifinder-analytics.tk',
+      domainId: '963ced5d-f56e-43eb-9fdb-7dc661aa1e2f',
+    },
+    {
+      ignoreLocalhost: true,
+      detailed: true,
+    },
+  );
+
   const cacheImages = async (imageArray: string[]) => {
     const promises = await imageArray.map((src) => {
       return new Promise((resolve, reject) => {
