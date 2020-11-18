72228890677ae58689e5bcef6a02e809447c809a
Romain Cascino <contact@romaincascino.com>
2020-11-18 07:51:08 +0100

    fix(server): Block quick play if the round is over

--- server/src/controllers/play/quickPlay.ts
+++ server/src/controllers/play/quickPlay.ts
@@ -3,26 +3,29 @@ import { getFormattedPlayers } from '../../core/room';
 import { Card, PlayedCards, Room, User } from '../../types';
 
 export const handleQuickPlay = (room: Room, user: User, { thrownCards }: PlayedCards): void => {
-  sendThrownCards(room.users, thrownCards);
+  // disallow quick play when the round is finished
+  if (!room.roundWinner) {
+    sendThrownCards(room.users, thrownCards);
 
-  const quickThrownCard = thrownCards[thrownCards.length - 1];
+    const quickThrownCard = thrownCards[thrownCards.length - 1];
 
-  user.hand.splice(
-    user.hand.findIndex(
-      (handCard: Card) =>
-        handCard.value == quickThrownCard.value && handCard.suit === quickThrownCard.suit,
-    ),
-    1,
-  );
-
-  // send the hand to the user who dropped the card
-  user.ws.send(JSON.stringify({ type: 'SET_PLAYER_HAND', hand: user.hand }));
-
-  // sync players to display other players cards
-  Object.entries(room.users).forEach(([, user]: [string, User]) => {
-    user.ws.send(JSON.stringify({ type: 'QUICK_PLAY_DONE' }));
-    user.ws.send(
-      JSON.stringify({ type: 'SET_OTHER_PLAYERS_CARDS', players: getFormattedPlayers(room) }),
+    user.hand.splice(
+      user.hand.findIndex(
+        (handCard: Card) =>
+          handCard.value == quickThrownCard.value && handCard.suit === quickThrownCard.suit,
+      ),
+      1,
     );
-  });
+
+    // send the hand to the user who dropped the card
+    user.ws.send(JSON.stringify({ type: 'SET_PLAYER_HAND', hand: user.hand }));
+
+    // sync players to display other players cards
+    Object.entries(room.users).forEach(([, user]: [string, User]) => {
+      user.ws.send(JSON.stringify({ type: 'QUICK_PLAY_DONE' }));
+      user.ws.send(
+        JSON.stringify({ type: 'SET_OTHER_PLAYERS_CARDS', players: getFormattedPlayers(room) }),
+      );
+    });
+  }
 };
