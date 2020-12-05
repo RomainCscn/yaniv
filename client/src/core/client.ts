4120e461deeea7b087472ab32a1ab5dbdbd0b4b4
Romain Cascino <contact@romaincascino.com>
2020-12-05 09:36:13 +0100

    fix(client): Rename room into roomId

--- client/src/core/client.ts
+++ client/src/core/client.ts
@@ -3,11 +3,11 @@ import { MessageAction, MessageActionType } from '../types';
 const client = new WebSocket(process.env.REACT_APP_WEB_SOCKET_URL);
 
 export const send = (
-  room: string,
+  roomId: string,
   { action, actionType }: { action: MessageAction; actionType?: MessageActionType },
   data?: object,
 ) => {
-  client.send(JSON.stringify({ action, actionType, room, ...data }));
+  client.send(JSON.stringify({ action, actionType, roomId, ...data }));
 };
 
 export default client;
